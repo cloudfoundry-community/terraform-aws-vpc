75a1db9f1e549404b25c6eecaeaf9dd5e93d13a6
David Bishop <david@gnuconsulting.com>
2014-10-14 12:28:23 -0400

    change provision.sh to use named variables

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+AWS_KEY_ID=$1
+AWS_ACCESS_KEY=$2
+REGION=$3
+VPC=$4
+BOSH_SUBNET=$5
+IPMASK=$6
+CP_IP=$7
+CF_SUBNET=$8
+
 cd $HOME
 sudo apt-get update
 sudo apt-get install -y git vim-nox build-essential libxml2-dev libxslt-dev libmysqlclient-dev libpq-dev libsqlite3-dev git
@@ -12,6 +21,12 @@ cat <<EOF > ~/.gemrc
 gem: --no-document
 EOF
 
+cat <<EOF > ~/.fog
+:default:
+    :aws_access_key_id: $AWS_KEY_ID
+    :aws_secret_access_key: $AWS_ACCESS_KEY
+    :region: $REGION
+EOF
 gem install bundler
 mkdir -p {bin,workspace/deployments,workspace/tools}
 pushd workspace/deployments
@@ -25,20 +40,13 @@ provider:
   name: aws
   credentials:
     provider: AWS
-    aws_access_key_id: $1
-    aws_secret_access_key: $2
-  region: $3
+    aws_access_key_id: $AWS_KEY_ID
+    aws_secret_access_key: $AWS_ACCESS_KEY
+  region: $REGION
 address:
-  vpc_id: $4
-  subnet_id: $5
-  ip: $6.2.4
-EOF
-
-cat <<EOF > ~/.fog
-:default:
-    :aws_access_key_id: $1
-    :aws_secret_access_key: $2
-    :region: $3
+  vpc_id: $VPC
+  subnet_id: $BOSH_SUBNET
+  ip: ${IPMASK}.2.4
 EOF
 
 bosh-bootstrap deploy
@@ -51,8 +59,8 @@ git clone http://github.com/cloudfoundry-community/cf-boshworkspace
 pushd cf-boshworkspace
 bundle install --path vendor/bundle
 mkdir -p ssh
-export CF_ELASTIC_IP=$7
-export SUBNET_ID=$8
+export CF_ELASTIC_IP=$CF_IP
+export SUBNET_ID=$CF_SUBNET
 export DIRECTOR_UUID=$(bundle exec bosh status | grep UUID | awk '{print $2}')
 for VAR in CF_ELASTIC_IP SUBNET_ID DIRECTOR_UUID
 do
