9420ba95ed286384b8d1e7615d8cd4e568b4abc2
David Bishop <david@gnuconsulting.com>
2014-11-04 16:42:01 -0500

    move cloudera into its own module

--- aws-vpc.tf
+++ aws-vpc.tf
@@ -1,7 +1,7 @@
 provider "aws" {
 	access_key = "${var.aws_access_key}"
 	secret_key = "${var.aws_secret_key}"
-	region = "${var.region}"
+	region = "${var.aws_region}"
 }
 
 resource "aws_vpc" "default" {
@@ -67,7 +67,7 @@ resource "aws_security_group" "nat" {
 }
 
 resource "aws_instance" "nat" {
-	ami = "${lookup(var.aws_nat_ami, var.region)}"
+	ami = "${lookup(var.aws_nat_ami, var.aws_region)}"
 	instance_type = "t2.small"
 	key_name = "${var.aws_key_name}"
 	security_groups = ["${aws_security_group.nat.id}"]
@@ -134,11 +134,6 @@ resource "aws_subnet" "microbosh" {
 	cidr_block = "${var.network}.2.0/24"
 }
 
-resource "aws_subnet" "cloudera" {
-	vpc_id = "${aws_vpc.default.id}"
-	cidr_block = "${var.network}.10.0/24"
-}
-
 # Routing table for private subnets
 
 resource "aws_route_table" "private" {
@@ -162,11 +157,6 @@ resource "aws_route_table_association" "cfruntime-2a-private" {
 
 resource "aws_route_table_association" "cfruntime-2b-private" {
 	subnet_id = "${aws_subnet.cfruntime-2b.id}"
-	route_table_id = "${aws_route_table.private.id}"
-}
-
-resource "aws_route_table_association" "cloudera-private" {
-	subnet_id = "${aws_subnet.cloudera.id}"
 	route_table_id = "${aws_route_table.private.id}"
 }
 
@@ -247,7 +237,7 @@ resource "aws_eip" "cf" {
 }
 
 resource "aws_instance" "bastion" {
-	ami = "${lookup(var.aws_ubuntu_ami, var.region)}"
+	ami = "${lookup(var.aws_ubuntu_ami, var.aws_region)}"
 	instance_type = "m1.medium"
 	key_name = "${var.aws_key_name}"
 	associate_public_ip_address = true
@@ -271,71 +261,18 @@ resource "aws_instance" "bastion" {
 	provisioner "remote-exec" {
 		inline = [
 			"chmod +x /home/ubuntu/provision.sh",
-			"/home/ubuntu/provision.sh ${var.aws_access_key} ${var.aws_secret_key} ${var.region} ${aws_vpc.default.id} ${aws_subnet.microbosh.id} ${var.network} ${aws_eip.cf.public_ip} ${aws_subnet.cfruntime-2a.id} ${aws_subnet.cfruntime-2a.availability_zone} ${aws_instance.bastion.availability_zone} ${aws_instance.bastion.id} ${aws_subnet.lb.id}",
+			"/home/ubuntu/provision.sh ${var.aws_access_key} ${var.aws_secret_key} ${var.aws_region} ${aws_vpc.default.id} ${aws_subnet.microbosh.id} ${var.network} ${aws_eip.cf.public_ip} ${aws_subnet.cfruntime-2a.id} ${aws_subnet.cfruntime-2a.availability_zone} ${aws_instance.bastion.availability_zone} ${aws_instance.bastion.id} ${aws_subnet.lb.id}",
 		]
   }
 
 }
 
-# Create the Cloudera Launcher Server & Security Group
-resource "aws_security_group" "cloudera_launcher" {
-	name = "cloudera_launcher"
-	description = "Allow SSH traffic from the internet"
-	vpc_id = "${aws_vpc.default.id}"
-
-	ingress {
-		from_port = 22
-		to_port = 22
-		protocol = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-
-	ingress {
-		from_port = 0
-		to_port = 65535
-		protocol = "tcp"
-		self = "true"
-	}
-
-	tags {
-		Name = "cloudera launcher"
-	}
-
-}
-
-resource "aws_instance" "cloudera_launcher" {
-	ami = "${lookup(var.aws_centos_ami, var.region)}"
-	instance_type = "t2.small"
-	key_name = "${var.aws_key_name}"
-	associate_public_ip_address = true
-	security_groups = ["${aws_security_group.cloudera_launcher.id}"]
-	subnet_id = "${aws_subnet.bastion.id}"
-
-	tags {
-		Name = "Cloudera Launcher"
-	}
-
-	connection {
-		user = "ec2-user"
-		key_file = "${var.aws_key_path}"
-	}
-
-	provisioner "file" {
-		source = "scripts/cloudera_launcher.sh"
-		destination = "/home/ec2-user/cloudera_launcher.sh"
-	}
-
-	provisioner "file" {
-		source = "${var.aws_key_path}"
-		destination = "/home/ec2-user/.ssh/cloudera.pem"
-	}
-
-	provisioner "remote-exec" {
-		inline = [
-			"chmod +x /home/ec2-user/cloudera_launcher.sh",
-			"chmod 0400 /home/ec2-user/.ssh/cloudera.pem",
-			"/home/ec2-user/cloudera_launcher.sh ${aws_subnet.cloudera.id} ${aws_security_group.cloudera_launcher.id} ${var.aws_key_name} ${var.hadoop_instance_count} ${var.hadoop_instance_type} ${lookup(var.aws_centos_ami, var.region)} ${var.aws_access_key} ${var.aws_secret_key}",
-		]
-	}
-
+module "cloudera" {
+  source = "./cloudera"
+  network = "${var.network}"
+  aws_centos_ami = "${lookup(var.aws_centos_ami, var.aws_region)}"
+  aws_key_name = "${var.aws_key_name}"
+  aws_vpc = "${aws_vpc.default.id}"
+  aws_route_table_private = "${aws_route_table.private.id}"
+  aws_subnet_bastion = "${aws_subnet.bastion.id}"
 }
--- variables.tf
+++ variables.tf
@@ -2,8 +2,8 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "aws_key_path" {}
 variable "aws_key_name" {}
-variable "region" {
-  default = "us-east-1"
+variable "aws_region" {
+  default = "us-west-2"
 }
 variable "aws_nat_ami" {
 	default = "ami-1d2b2958"
